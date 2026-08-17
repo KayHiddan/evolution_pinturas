d63b3e7a3d0893553ed893f67c01cbaf84d37a85
theus021 <mateusoliver2109@hotmail.com>
2026-08-17 13:21:04 -0300

    implements_js_header

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,7 +2,36 @@ $(document).ready(() => {
     $('#mobile_menu_button').click(function () {
         $('#mobile_menu').toggleClass('active');
         $('#mobile_menu_button').find('i').toggleClass('fa-bars fa-times');
+   
+      });
+
+    const section = $('section');
+    const navItems = $('nav_item');
+
+    $(window).on('scroll', function () {
+      const header = $('header')
+      const scrollposition = $(window).scrolltop() - header.outerheight();
+
+      let activeSectionIndex = 0;
+
+      if(scrollposition <= 0 ){
+        header.css('box-shadow', 'none');
+      } else {
+        header.css('box-shadow', '5px 1px 7px rgba(0, 0, 0, 0.1');
+      }
+
+      section.each(function(i){
+        const section = $(this); 
+        const sectionTop = section.offset().top - 74
+        const sectionBottom = sectionTop + section.outerheight();
+
+        if (scrollposition >= sectionTop && scrollposition + sectionBottom){
+          activeSectionIndex = i;
+          return false
+        }
+      })
     });
+
 });
 
 const swiper = new Swiper('.swiper_container', {
